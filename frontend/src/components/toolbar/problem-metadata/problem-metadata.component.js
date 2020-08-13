f7ec54c942bf1f29696a7b8cadf082b4d0319463
Sig00rd <hamletolog@gmail.com>
2020-08-14 01:34:21 +0200

    Add inputs for problem metadata component and the necessary dependencies

--- frontend/src/components/toolbar/problem-metadata/problem-metadata.component.js
+++ frontend/src/components/toolbar/problem-metadata/problem-metadata.component.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import DateFnsUtils from "@date-io/date-fns";
+import plLocale from "date-fns/locale/pl";
+import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
 
 function ProblemMetadataComponent() {
-    return (
-        <div>
-            "ProblemMetadataComponent works!"
-        </div>
-    );
+  const [selectedDate, handleDateChange] = useState(new Date());
+
+  return (
+    <form autoComplete="off">
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={plLocale}>
+        <DatePicker
+          variant="inline"
+          openTo="year"
+          views={["year", "month"]}
+          label="Z miesiąca"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+      </MuiPickersUtilsProvider>
+      <TextField id="children" label="Ilość dzieci" />
+      <TextField id="nurses" label="Ilość pielęgniarek" />
+      <TextField id="babysitters" label="Ilość opiekunek" />
+    </form>
+  );
 }
 
 export default ProblemMetadataComponent;
