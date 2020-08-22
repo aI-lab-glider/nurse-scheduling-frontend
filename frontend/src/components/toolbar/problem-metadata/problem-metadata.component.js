a25eca9f818c7c2f91520ba0df1bcac493eb42f5
Sig00rd <hamletolog@gmail.com>
2020-08-22 17:54:44 +0200

    Wire up state for metadata component and make inputs required to submit form

--- frontend/src/components/toolbar/problem-metadata/problem-metadata.component.js
+++ frontend/src/components/toolbar/problem-metadata/problem-metadata.component.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import DateFnsUtils from "@date-io/date-fns";
 import plLocale from "date-fns/locale/pl";
@@ -6,11 +7,20 @@ import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
 
 function ProblemMetadataComponent() {
   const [selectedDate, handleDateChange] = useState(new Date());
+  const [numberOfChildren, setNumberOfChildren] = useState("");
+  const [numberOfNurses, setNumberOfNurses] = useState("");
+  const [numberOfSitters, setNumberOfSitters] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Metadata form submitted!");
+  };
 
   return (
-    <form autoComplete="off">
+    <form onSubmit={handleSubmit} autoComplete="off">
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={plLocale}>
         <DatePicker
+          required
           variant="inline"
           openTo="year"
           views={["year", "month"]}
@@ -19,9 +29,30 @@ function ProblemMetadataComponent() {
           onChange={handleDateChange}
         />
       </MuiPickersUtilsProvider>
-      <TextField id="children" label="Ilość dzieci" />
-      <TextField id="nurses" label="Ilość pielęgniarek" />
-      <TextField id="babysitters" label="Ilość opiekunek" />
+      <TextField
+        required
+        id="children"
+        label="Ilość dzieci"
+        value={numberOfChildren}
+        onChange={(e) => setNumberOfChildren(e.target.value)}
+      />
+      <TextField
+        required
+        id="nurses"
+        label="Ilość pielęgniarek"
+        value={numberOfNurses}
+        onChange={(e) => setNumberOfNurses(e.target.value)}
+      />
+      <TextField
+        required
+        id="sitters"
+        label="Ilość opiekunek"
+        value={numberOfSitters}
+        onChange={(e) => setNumberOfSitters(e.target.value)}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Poprawić harmonogram
+      </Button>
     </form>
   );
 }
