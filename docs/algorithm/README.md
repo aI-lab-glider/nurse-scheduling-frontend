9a2c54f8955e93d906e5cdd905d7418adb413551
Bohdan Forostianyi <qwebeck5@gmail.com>
2020-09-05 13:37:52 +0200

    Merge feature/metadata in develop

--- docs/algorithm/README.md
+++
@@ -1,1 +0,0 @@
-## Things to know about alhorithm
--- frontend/src/components/toolbar/toolbar.component.js
+++ frontend/src/components/toolbar/toolbar.component.js
@@ -3,13 +3,13 @@ import { ImportButtonsComponent } from "./import-buttons/index";
 import ProblemMetadataComponent from "./problem-metadata";
 import "./toolbar.component.css";
 
-function ToolbarComponent() {
-  return (
-    <div className="header">
-      <ImportButtonsComponent />
-      <ProblemMetadataComponent />
-    </div>
-  );
+function ToolbarComponent () {
+    return (
+        <div className="flex f-row">
+            <ImportButtonsComponent />
+            <ProblemMetadataComponent />
+        </div>
+    )
 }
 
 export default ToolbarComponent;
