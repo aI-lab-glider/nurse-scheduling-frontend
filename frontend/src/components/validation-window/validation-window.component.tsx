f84d0567d7e8926d438a34cc1547e46b36fc9786
Sig00rd <hamletolog@gmail.com>
2020-09-12 13:41:10 +0200

    Add single error view

--- frontend/src/components/validation-window/validation-window.component.tsx
+++ frontend/src/components/validation-window/validation-window.component.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import Alert from "@material-ui/lab/Alert";
+
 export function ValidationWindowComponent() {
-  return <div>"ValidationWindowComponent works !"</div>;
+  //#region view
+  function error(message) {
+    return <Alert severity="warning">{message}</Alert>;
+  }
+
+  return (
+    <div>
+      {error("Brakuje 3 pielęgniarek na zmianie dziennej dnia 17!")}
+      {error("bleblepleple")}
+    </div>
+  );
+  //#endregion
 }
