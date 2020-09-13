fbb5fe441434df603748b13ea31b98295fb24829
Sig00rd <hamletolog@gmail.com>
2020-09-13 18:00:05 +0200

    Add initial hooks wireup for validation window component

--- frontend/src/components/validation-window/validation-window.component.tsx
+++ frontend/src/components/validation-window/validation-window.component.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import Alert from "@material-ui/lab/Alert";
+import { ApplicationStateModel } from "../../state/models/application-state.model";
 
 export function ValidationWindowComponent() {
+  //#region members
+  const [errors, setErrors] = useState();
+  //#endregion
+
+  //#region state interaction
+  const errors_received = useSelector((state: ApplicationStateModel) => state.scheduleErrors);
+
+  useEffect(() => {
+    if (errors_received) {
+      console.log("Got errors!");
+    }
+  }, [errors_received]);
+
+  //#endregion
+
   //#region view
   function error(message) {
     return <Alert severity="warning">{message}</Alert>;
