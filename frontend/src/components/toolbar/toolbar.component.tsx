276adcdddb702b306f7fde3ba2670546fbed1cbe
Sig00rd <hamletolog@gmail.com>
2020-09-22 20:39:23 +0200

    Add button to show drawer and merge drawer component with it

--- frontend/src/components/toolbar/toolbar.component.tsx
+++ frontend/src/components/toolbar/toolbar.component.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { ImportButtonsComponent } from "./import-buttons/import-buttons.component";
 import { ProblemMetadataComponent } from "./problem-metadata/problem-metadata.component";
 import "./toolbar.component.css";
+import ValidationWindowButtonComponent from "./validation-window-button/validation-window-button.component";
 
 export function ToolbarComponent() {
   return (
     <div className="header">
       <ImportButtonsComponent />
+      <ValidationWindowButtonComponent />
       <ProblemMetadataComponent />
     </div>
   );
