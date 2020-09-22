6139c0aaaf600c9685fddee71b5d1bde28344ba1
Sig00rd <hamletolog@gmail.com>
2020-09-23 00:19:36 +0200

    Rename component file and catalog as well

--- frontend/src/components/toolbar/toolbar.component.tsx
+++ frontend/src/components/toolbar/toolbar.component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ImportButtonsComponent } from "./import-buttons/import-buttons.component";
 import { ProblemMetadataComponent } from "./problem-metadata/problem-metadata.component";
 import "./toolbar.component.css";
-import ValidationDrawerComponent from "./validation-window-button/validation-window-button.component";
+import ValidationDrawerComponent from "./validation-drawer/validation-drawer.component";
 
 export function ToolbarComponent() {
   return (
