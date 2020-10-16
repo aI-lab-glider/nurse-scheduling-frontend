1a208688bb30439c7845624ff3e4003acff7b743
Tomasz Pecak <pecatoma@gmail.com>
2020-10-16 18:47:20 +0200

    Run prettier for all tsx,ts,css, files

--- frontend/src/components/table/schedule/sections/date-section/date-section.component.tsx
+++ frontend/src/components/table/schedule/sections/date-section/date-section.component.tsx
@@ -3,7 +3,5 @@ import { BaseSectionComponent } from "../base-section/base-section.component";
 import { DateSectionOptions } from "./date-section.options";
 
 export function DateSectionComponent(options: DateSectionOptions) {
-  return (
-    <BaseSectionComponent {...options}/>
-  );
+  return <BaseSectionComponent {...options} />;
 }
