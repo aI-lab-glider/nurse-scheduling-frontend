3e9d3b77ae6f9d9f72bc9ab668890ae6a232d105
kaplonpaulina <kaplonpaulina97@gmail.com>
2020-11-06 11:33:12 +0100

    add electron

---
+++ frontend/public/electron.js
@@ -0,0 +1,26 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({show: false});
+    mainWindow.maximize();
+    mainWindow.show();
+    mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
