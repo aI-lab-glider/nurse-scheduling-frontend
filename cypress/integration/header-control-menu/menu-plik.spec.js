62f2b59c182fbb6f272dd3ac7a1283b63a8351fc
Maciej Bielech <maciej.bielech@u2i.com>
2020-11-09 22:21:54 +0100

    TASK-11 Add test: Exporting file and loading exported file

--- cypress/integration/header-control-menu/menu-plik.spec.js
+++ cypress/integration/header-control-menu/menu-plik.spec.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
+const fs = require("fs");
 
 context("Menu 'plik'", () => {
   beforeEach(() => {
     cy.visit(`${Cypress.env("baseUrl")}`);
+    cy.contains("Plik").click();
   });
 
   describe("File button", () => {
     it("Should open options menu", () => {
-      cy.contains("Plik").click();
       cy.contains("Wczytaj");
       cy.contains("Zapisz jako...");
     });
@@ -15,8 +16,25 @@ context("Menu 'plik'", () => {
 
   describe("Load schedule", () => {
     it("Should be able to load and show the schedule", () => {
-      cy.contains("Plik").click();
       cy.get('[data-cy="file-input"]').attachFile("example.xlsx");
+      cy.contains("Dni miesiąca");
+    });
+  });
+
+  describe("Export schedule and load exported", () => {
+    it("Should be able to export and load exported schedule", () => {
+      cy.get('[data-cy="file-input"]').attachFile("example.xlsx");
+      cy.contains("Zapisz jako...").click();
+      cy.exec('whoami').then((result) => {
+        const user = result.stdout;
+        cy.readFile(`/Users/${user}/Downloads/grafik.xlsx`).then((file) =>{
+          cy.writeFile("./cypress/fixtures/grafik.xlsx", file);
+        });
+      })
+      cy.contains("28").click();
+      cy.get('[class="cell-input"').type("1");
+      cy.contains("Plik").click();
+      cy.get('[data-cy="file-input"]').attachFile("grafik.xlsx");
       cy.contains("Dni miesiąca");
     });
   });
