065e9302ff6fd23200d18618b7c2c21a0542fd6b
kaplonpaulina <kaplonpaulina97@gmail.com>
2020-11-10 21:28:50 +0100

    TASK-3 add electron

---
+++ public/electron.js
@@ -0,0 +1,29 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+const dotenv = require('dotenv');
+let mainWindow;
+
+const env = dotenv.config().parsed;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({show: false});
+    mainWindow.maximize();
+    mainWindow.show();
+    mainWindow.loadURL(isDev ? `http://localhost:${env.PORT}` : `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
