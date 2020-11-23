ee20cffbf9ae684bef63456e2991dacf7ec1b5a8
dimonium_239 <dimon041220000@gmail.com>
2020-11-23 16:03:16 +0100

    TASK-61 getWorkersCount added to ShiftHelper

---
+++ cypress/integration/unit/helpers/shift.helper.spec.ts
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+import { DataRow } from "../../../../src/logic/schedule-logic/data-row";
+import { ShiftHelper } from "../../../../src/helpers/shifts.helper"
+
+type TestCase = { arr: DataRow[]; day: number; exp: number };
+
+const testData = [
+    new DataRow("", ["R","DN","W"]),
+    new DataRow("", ["P","D","W"]),
+    new DataRow("", ["D","W","W"])
+    ]
+
+var testCases: TestCase[] = [
+    {
+        arr: testData,
+        day: 0,
+        exp: 3,
+    },
+    {
+        arr: testData,
+        day: 1,
+        exp: 2,
+    },
+    {
+        arr: testData,
+        day: 2,
+        exp: 0,
+    }
+  ];
+  
+describe("ShiftHelper", () => {
+    testCases.forEach((testCase) => {
+        describe("getWorkersCount", () => {
+            const matrix = testCase.arr.map(d => d.rowData(false, false));
+            it(`should return ${testCase.exp} for day ${testCase.day} and array ${matrix}`, () => {
+                const amount = ShiftHelper.getWorkersCount(testCase.day, testData);
+                expect(amount).to.equal(testCase.exp);
+            });
+        });
+    });
+});
