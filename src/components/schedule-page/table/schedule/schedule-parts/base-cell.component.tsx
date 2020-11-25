8ee5305ee25965cd09bb2ba2e3fa12b3d26c5d42
Ehevi <aga.cyra@wp.pl>
2020-11-25 12:12:22 +0100

    TASK-27 dropdown: popper & autocomplete both work, but got some trouble with updating the value in the cell. please help, nothing i've tried so far works

--- src/components/schedule-page/table/schedule/schedule-parts/base-cell.component.tsx
+++ src/components/schedule-page/table/schedule/schedule-parts/base-cell.component.tsx
@@ -1,7 +1,81 @@
 import React from "react";
 import { ColorHelper } from "../../../../../helpers/colors/color.helper";
 import { CellColorSet } from "../../../../../helpers/colors/cell-color-set.model";
+import { ShiftCode } from "../../../../../common-models/shift-info.model";
+import { createStyles, makeStyles, Popper, TextField, Theme } from "@material-ui/core";
+import { useAutocomplete } from "@material-ui/lab";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    label: {
+      display: "block",
+    },
+    listbox: {
+      width: 200,
+      margin: 0,
+      padding: 0,
+      zIndex: 1,
+      position: "absolute",
+      listStyle: "none",
+      backgroundColor: theme.palette.background.paper,
+      overflow: "auto",
+      maxHeight: 200,
+      border: "1px solid rgba(0,0,0,.25)",
+      '& li[data-focus="true"]': {
+        backgroundColor: "#4a8df6",
+        color: "white",
+        cursor: "pointer",
+      },
+      "& li:active": {
+        backgroundColor: "#2977f5",
+        color: "white",
+      },
+    },
+  })
+);
+interface Opt {
+  onKeyDown: (value) => void;
+}
+function InputWithOptions({ onKeyDown }: Opt) {
+  const ShiftCodeTranslation = [
+    { name: "rano: 7-15", symbol: "R", code: ShiftCode.R },
+    { name: "popołudnie: 15-19", symbol: "P", code: ShiftCode.P },
+    { name: "dzień: 7-19", symbol: "D", code: ShiftCode.D },
+    { name: "noc: 19-7", symbol: "N", code: ShiftCode.N },
+    { name: "dzień + noc: 7-7", symbol: "DN", code: ShiftCode.DN },
+    { name: "popołudnie + noc: 15-7", symbol: "PN", code: ShiftCode.PN },
+    { name: "wolne", symbol: "W", code: ShiftCode.W },
+    { name: "urlop", symbol: "U", code: ShiftCode.U },
+    { name: "L4", symbol: "L4", code: ShiftCode.L4 },
+  ];
+  const classes = useStyles();
+  const {
+    getRootProps,
+    getInputLabelProps,
+    getInputProps,
+    getListboxProps,
+    getOptionProps,
+    groupedOptions,
+  } = useAutocomplete({
+    open: true,
+    options: ShiftCodeTranslation,
+    getOptionLabel: (option) => option.name,
+  });
+  return (
+    <div>
+      <div {...getRootProps()}>
+        <input className="cell-input" autoFocus={true} {...getInputProps()} onKeyDown={onKeyDown} />
+      </div>
+      {groupedOptions.length > 0 ? (
+        <ul className={classes.listbox} {...getListboxProps()}>
+          {groupedOptions.map((option, index) => (
+            <li {...getOptionProps({ option, index })}>{option.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
 export interface BaseCellOptions {
   index: number;
   value: string;
@@ -15,7 +89,8 @@ export interface BaseCellOptions {
   onBlur?: () => void;
 }
 
-function BaseCellComponentF({
+export function BaseCellComponent({
+  index,
   value,
   style = ColorHelper.DEFAULT_COLOR_SET,
   isBlocked,
@@ -25,22 +100,20 @@ function BaseCellComponentF({
   onContextMenu,
   onClick,
   onBlur,
-}: BaseCellOptions): JSX.Element {
-  const inputRef = React.createRef<HTMLInputElement>();
-
-  function handleContextMenu(e: React.MouseEvent): void {
+}: BaseCellOptions) {
+  function handleContextMenu(e: React.MouseEvent) {
     e.preventDefault();
     onContextMenu && onContextMenu();
   }
+  function _onKeyDown(e: React.KeyboardEvent) {
+    onKeyDown && onKeyDown(e.currentTarget.nodeValue || value, e);
+  }
 
+  //  #region view
   return (
     <td
-      className={"cell"}
-      onClick={(): void => {
-        if (!isBlocked && onClick) {
-          onClick();
-        }
-      }}
+      className={`cell`}
+      onClick={() => !isBlocked && onClick && onClick()}
       onContextMenu={handleContextMenu}
       style={{
         color: style.textColor.toString(),
@@ -49,29 +122,15 @@ function BaseCellComponentF({
             ? ColorHelper.getHighlightColor().toString()
             : style.backgroundColor.toString(),
       }}
-      onKeyDown={(e): void => {
-        onKeyDown && onKeyDown(inputRef.current?.value || value, e);
-      }}
-      onBlur={(e): void => {
+      onKeyDown={_onKeyDown}
+      onBlur={(e) => {
         onBlur && onBlur();
       }}
     >
-      {isPointerOn && !isBlocked && (
-        <input defaultValue={value} autoFocus={true} ref={inputRef} className="cell-input" />
-      )}
+      {isPointerOn && !isBlocked && <InputWithOptions onKeyDown={_onKeyDown} />}
 
       {(!isPointerOn || (isPointerOn && isBlocked)) && <span>{value}</span>}
     </td>
   );
+  //#endregion
 }
-
-export const BaseCellComponent: React.FC<BaseCellOptions> = React.memo(
-  BaseCellComponentF,
-  (prev, next) => {
-    const areEqual =
-      prev.value === next.value &&
-      prev.isSelected === next.isSelected &&
-      next.isPointerOn === prev.isPointerOn;
-    return areEqual;
-  }
-);
