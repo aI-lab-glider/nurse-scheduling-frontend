5b2a3163ba7c82155dcfb58c2ad9d6728f7e7e2a
Dushess0 <ilyabannila@gmail.com>
2020-11-26 20:20:25 +0100

    used logic from Task-61 (getWorkersCount)

--- src/logic/providers/foundation-info-provider.model.ts
+++ src/logic/providers/foundation-info-provider.model.ts
@@ -1,6 +1,7 @@
 import { ShiftInfoModel } from "../../common-models/shift-info.model";
 import { WorkerType } from "../../common-models/worker-info.model";
 import { ArrayHelper } from "../../helpers/array.helper";
+import { ShiftHelper } from "../../helpers/shifts.helper";
 import { ChildrenInfoProvider } from "./children-info-provider.model";
 import { ExtraWorkersInfoProvider } from "./extra-workers-info-provider.model";
 import { Sections } from "./schedule-provider.model";
@@ -27,10 +28,7 @@ export abstract class FoundationInfoProvider {
     return this.sections.ExtraWorkersInfo.extraWorkers;
   }
   getWorkersCount(workerType: WorkerType): number[] {
-    return ArrayHelper.arrayOfCount(
-      (Object.values(this.sections.NurseInfo.workerShifts)[0] ?? []).length,
-      7
-    );
+    return ShiftHelper.getWorkersCount(this.workersShifts);
   }
 
   constructor(protected sections: FoundationInfoOptions) {}
