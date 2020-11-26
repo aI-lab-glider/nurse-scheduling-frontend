9dcdeab084d3088dae7e103238807b42122a13ac
vvlovsky <slvtrshkv@gmail.com>
2020-11-27 01:36:50 +0200

    TASK-105 init

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,14 @@ import SchedulePage from "./components/schedule-page/schedule-page.component";
 import ManagementPage from "./components/workers-page/management-page.component";
 import { CustomGlobalHotKeys } from "./components/common-components/tools/globalhotkeys.component";
 import Header from "./components/common-components/header/header";
+import RouteButtonsComponent from "./components/common-components/route-buttons/route-buttons.component";
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
       <CustomGlobalHotKeys />
       <Header />
+      <RouteButtonsComponent />
       <Switch>
         <Route path="/" component={SchedulePage} exact />
         <Route path="/management" component={ManagementPage} />
