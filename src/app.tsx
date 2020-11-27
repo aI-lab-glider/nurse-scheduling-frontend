120bf9515f65b69e8f2bb90e4b76beea822a4c33
vvlovsky <slvtrshkv@gmail.com>
2020-11-27 05:11:19 +0200

    TASK-105 Header renamed to HeaderComponent

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import SchedulePage from "./components/schedule-page/schedule-page.component";
 import ManagementPage from "./components/workers-page/management-page.component";
 import { CustomGlobalHotKeys } from "./components/common-components/tools/globalhotkeys.component";
-import Header from "./components/common-components/header/header";
+import HeaderComponent from "./components/common-components/header/header.component";
 import RouteButtonsComponent from "./components/common-components/route-buttons/route-buttons.component";
 
 const routes = { Plan: SchedulePage, Zarządzanie: WorkersPage };
@@ -12,7 +12,7 @@ function App(): JSX.Element {
   return (
     <React.Fragment>
       <CustomGlobalHotKeys />
-      <Header />
+      <HeaderComponent />
       <RouteButtonsComponent components={routes} />
     </React.Fragment>
   );
