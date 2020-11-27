a305f4855c2ab6c7cbed87ae0f891d8211e39a4f
vvlovsky <slvtrshkv@gmail.com>
2020-11-27 18:17:02 +0200

    TASK-105 Tabs Customization

--- src/components/common-components/route-buttons/route-buttons.component.tsx
+++ src/components/common-components/route-buttons/route-buttons.component.tsx
@@ -3,25 +3,56 @@ import { Tab, withStyles } from "@material-ui/core";
 import TabContext from "@material-ui/lab/TabContext";
 import TabList from "@material-ui/lab/TabList";
 import TabPanel from "@material-ui/lab/TabPanel";
+import { makeStyles } from "@material-ui/core/styles";
+import ScssVars from "../../../assets/styles/styles/custom/route-buttons.module.scss";
 
 interface RouteButtonsComponent {
   components: { [key: string]: React.FC };
 }
 
+const useStyles = makeStyles(() => ({
+  indicatorStyle: {
+    backgroundColor: ScssVars.ic,
+    height: 3,
+    opacity: 1,
+    outline: "none",
+  },
+
+  tabStyle: {
+    opacity: 1,
+    outline: "none",
+  },
+}));
+
 const StyledTab: any = withStyles((theme) => ({
   root: {
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: theme.typography.pxToRem(16),
-    lineHeight: theme.typography.pxToRem(28),
-    color: "$primary-text-color",
+    textTransform: "none",
+    color: ScssVars.tc,
+    outline: "none",
+    fontWeight: theme.typography.fontWeightMedium,
+    fontSize: "16",
+    opacity: 1,
+    fontFamily: ["Roboto"].join(","),
+    "&:hover": {
+      color: ScssVars.ic,
+      opacity: 1,
+      outline: "none",
+    },
+    "&$selected": {
+      color: ScssVars.tc,
+      outline: "none",
+      fontWeight: theme.typography.fontWeightBold,
+    },
   },
-  label: {
-    textTransform: "capitalize",
+
+  selected: {
+    outline: "none",
   },
 }))((props) => <Tab disableRipple {...props} />);
 
 export default function RouteButtonsComponent({ components }: RouteButtonsComponent): JSX.Element {
   const [value, setValue] = React.useState("0");
+  const classes = useStyles();
   const handleChange = (event: React.ChangeEvent<{}>, newValue: string): void => {
     setValue(newValue);
   };
@@ -30,9 +61,16 @@ export default function RouteButtonsComponent({ components }: RouteButtonsCompon
     <>
       <div>
         <TabContext value={value}>
-          <TabList onChange={handleChange} indicatorColor={"primary"}>
+          <TabList classes={{ indicator: classes.indicatorStyle }} onChange={handleChange}>
             {Object.keys(components).map((name, index) => {
-              return <StyledTab key={name} label={name} value={index.toString()} />;
+              return (
+                <StyledTab
+                  className={classes.tabStyle}
+                  key={name}
+                  label={name}
+                  value={index.toString()}
+                />
+              );
             })}
           </TabList>
 
