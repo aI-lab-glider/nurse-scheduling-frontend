0b4b0e0ddfcd5fb9d5331f9ac3993eb42bddd6a3
kaplonpaulina <kaplonpaulina97@gmail.com>
2020-11-28 03:41:33 +0100

    TASK-26 timetable component

---
+++ src/components/timetable/timetable.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TimeTableSection } from "./timetable-section.component";
+
+export function TimeTableComponent(): JSX.Element {
+  return (
+    <div>
+      <TimeTableSection />
+    </div>
+  );
+}
