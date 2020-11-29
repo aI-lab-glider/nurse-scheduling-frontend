bf2c3af28fe6b991236cf29e1ac56ab8357905ac
dimonium_239 <dimon041220000@gmail.com>
2020-11-29 23:56:05 +0100

    TASK-88 cypress environment variables fixed

--- cypress/integration/e2e/header-control-menu/menu-plik.spec.js
+++ cypress/integration/e2e/header-control-menu/menu-plik.spec.js
@@ -2,7 +2,7 @@
 
 context("Menu 'plik'", () => {
   beforeEach(() => {
-    cy.visit(`http://localhost:3000/`);
+    cy.visit(`${Cypress.env("baseUrl")}`);
   });
 
   describe("File button", () => {
