7e8f7459e5d55cecb80958a3bbdd3b564c1e4ad1
vvlovsky <slvtrshkv@gmail.com>
2020-12-01 01:20:02 +0200

    TASK-29 ProblemMetadataComponent removed

--- src/components/schedule-page/toolbar.component.tsx
+++ src/components/schedule-page/toolbar.component.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { MonthSwitchComponent } from "../common-components";
 import { ImportButtonsComponent } from "./import-buttons/import-buttons.component";
-import { ProblemMetadataComponent } from "./problem-metadata/problem-metadata.component";
 import ValidationDrawerComponent from "./validation-drawer/validation-drawer.component";
 
 export function ToolbarComponent(): JSX.Element {
@@ -10,7 +9,6 @@ export function ToolbarComponent(): JSX.Element {
       <ImportButtonsComponent />
       <ValidationDrawerComponent />
       <MonthSwitchComponent />
-      <ProblemMetadataComponent />
     </div>
   );
 }
