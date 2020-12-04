875414dcc794c29af28c2fb9c77a7a029105288a
Maciej Bielech <maciej.bielech@gmail.com>
2020-12-04 14:09:00 +0100

    TASK-118 Load example schedule data when dev mode is on (#99)

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SchedulePage from "./components/schedule-page/schedule-page.component";
 import ManagementPage from "./components/workers-page/management-page.component";
 import RouteButtonsComponent from "./components/common-components/route-buttons/route-buttons.component";
 import { CustomGlobalHotKeys, HeaderComponent } from "./components/common-components";
+import { ScheduleDataActionType } from "./state/reducers/schedule-data.reducer";
+import { ActionModel } from "./state/models/action.model";
+import { ScheduleDataModel } from "./common-models/schedule-data.model";
+import { useDispatch } from "react-redux";
+import schedule from "./assets/devMode/schedule.js";
 
 interface TabData {
   label: string;
@@ -15,6 +20,16 @@ const tabs: TabData[] = [
 ];
 
 function App(): JSX.Element {
+  const scheduleDispatcher = useDispatch();
+  useEffect(() => {
+    if (process.env.REACT_APP_DEV_MODE === "true") {
+      scheduleDispatcher({
+        type: ScheduleDataActionType.ADD_NEW,
+        payload: schedule,
+      } as ActionModel<ScheduleDataModel>);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <CustomGlobalHotKeys />
