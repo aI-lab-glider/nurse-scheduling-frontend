405af37eac9e8669c2b4ee0932a306021b94981e
Tomasz Pecak <pecatoma@gmail.com>
2020-12-09 15:59:24 +0100

    Create test for comparator

--- src/helpers/comparator-helper.tsx
+++ src/helpers/comparator-helper.tsx
@@ -2,6 +2,9 @@ export type ComparatorValues = 1 | 0 | -1;
 export type Order = "asc" | "desc";
 
 export class ComparatorHelper {
+  /**
+   * Compare 2 objects in descending order. For complex types returns 0.
+   */
   public static descendingComparator<T>(a: T, b: T, orderBy: keyof T): ComparatorValues {
     if (b[orderBy] < a[orderBy]) {
       return -1;
@@ -12,13 +15,22 @@ export class ComparatorHelper {
     }
   }
 
+  /**
+   * Creates comparator function which can compare 2 object of type T in specified order (ascending or descending) by one of T key.
+   */
   public static getComparator<T>(order: Order, orderBy: keyof T): (a: T, b: T) => ComparatorValues {
     return order === "desc"
       ? (a, b): ComparatorValues => ComparatorHelper.descendingComparator(a, b, orderBy)
       : (a, b): ComparatorValues =>
           -ComparatorHelper.descendingComparator(a, b, orderBy) as ComparatorValues;
   }
-
+  /**
+   * Sorts array of objects in ascending or descending order by one of the object's key.
+   * @param array - array of objects T
+   * @param order - order of sorting (asc or desc)
+   * @param orderBy - key of object T by which order will be determine
+   * If type of orderBy is complex result array order will be the same as input order
+   */
   public static stableSort<T>(array: T[], order: Order, orderBy: keyof T): T[] {
     const stabilizedArray = array.map((el, index) => [el, index] as [T, number]);
     const comparator = ComparatorHelper.getComparator(order, orderBy);
