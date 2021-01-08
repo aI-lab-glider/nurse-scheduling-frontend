5bf185e2d8bcfa3a7b4ef55bae0bf7eb47d36a50
vvlovsky <slvtrshkv@gmail.com>
2021-01-09 00:40:01 +0200

    TASK-181-fixes

--- cypress/integration/e2e/table/schedule-modes.spec.ts
+++ cypress/integration/e2e/table/schedule-modes.spec.ts
@@ -7,7 +7,7 @@ import { WorkerType } from "../../../../src/common-models/worker-info.model";
 const testedCell = {
   workerType: WorkerType.NURSE,
   workerIdx: 0,
-  shiftIdx: 6,
+  shiftIdx: 4,
   initialShiftCode: ShiftCode.U,
   desiredShiftCode: ShiftCode.W,
 };
