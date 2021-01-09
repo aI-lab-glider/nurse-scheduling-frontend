867ee65314717e48e9500986fbb512549c168bb0
Dimonium-239 <dymitr.kuzmin@gmail.com>
2021-01-09 23:00:03 +0100

    TASK-183 finished

--- src/components/workers-page/shifts-tab/shift-edit-drawer.component.tsx
+++ src/components/workers-page/shifts-tab/shift-edit-drawer.component.tsx
@@ -2,82 +2,108 @@ import TextField from "@material-ui/core/TextField";
 import React, { useState } from "react";
 import { Button } from "../../common-components/button-component/button.component";
 import { ColorSelector } from "../../common-components/color-selector/color-selector.component";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { MuiPickersUtilsProvider, TimePicker } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import { KeyboardTimePicker } from "@material-ui/pickers";
 import { FormControl, RadioGroup, FormControlLabel, Radio } from "@material-ui/core";
-import { Shift } from "../../../common-models/shift-info.model";
+import { Shift, shifts } from "../../../common-models/shift-info.model";
+import { AcronymGenerator } from "../../../helpers/acronym-generator.helper";
 
 export default function ShiftEditDrower(): JSX.Element {
+  const shiftNames = Object.values(shifts).map((shift) => shift.name);
+  const [shiftName, setShiftName] = useState("");
+  const [shiftCode, setShiftCode] = useState("");
+  const [isInShiftNames, checkShiftName] = useState(false);
+
+  const shiftNameTextFieldOnChange = (shiftNameActual: string): void => {
+    setShiftName(shiftNameActual);
+    setShiftCode(AcronymGenerator.generate(shiftNameActual));
+    checkShiftName(shiftNames.includes(shiftNameActual));
+  };
+
   const [valueTimeStart, onChangeTimeStart] = useState<Date | null>(
     new Date("2021-01-01T23:00:00.000Z")
   );
   const [valueTimeEnd, onChangeTimeEnd] = useState<Date | null>(
     new Date("2021-01-01T23:00:00.000Z")
   );
-  const [value, setValue] = useState("working");
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const [valueRadio, setValueRadio] = useState("working");
+  const radioChange = (shiftType: string): void => {
+    setValueRadio(shiftType);
   };
 
-  const [colorPicked, setPicked] = useState("000000");
-  const colorClicked = (event: string) => setPicked(event);
+  const [colorPicked, setPicked] = useState("");
+  const colorClicked = (colorChosen: string): void => setPicked(colorChosen);
 
   const [newShift, setShift] = useState<Shift | null>(null);
-  const setNewShift = (event: Shift) => {
-    setShift(event);
-  };
+  const setNewShift = (createdShift: Shift): void => setShift(createdShift);
+
   return (
     <>
       <div className={"edit-field"}>
         <h4>Nazwa zmiany</h4>
-        <TextField type="text" placeholder="Nazwa zmiany" />
+        <TextField
+          type="text"
+          placeholder="Nazwa zmiany"
+          onChange={(event): void => shiftNameTextFieldOnChange(event.target.value)}
+          helperText={isInShiftNames ? "Zmiana z taką nazwą już istnieje" : ""}
+          error={isInShiftNames}
+        />
+
         <br />
+
         <h4>Typ zmiany</h4>
         <FormControl component="fieldset">
           <RadioGroup
             row
             aria-label="shiftType"
-            name="shiftType1"
-            value={value}
-            onChange={handleChange}
+            name="shiftType"
+            value={valueRadio}
+            onChange={(event): void => radioChange(event.target.value)}
           >
             <FormControlLabel value="working" control={<Radio />} label="Pracująca" />
             <FormControlLabel value="not_working" control={<Radio />} label="Niepracująca" />
           </RadioGroup>
         </FormControl>
         <br />
+
         <h4>Godziny zmiany</h4>
         <div className={"time-range"}>
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardTimePicker
+            <TimePicker
               label=""
               placeholder="00:00"
               ampm={false}
-              mask="__:__"
               value={valueTimeStart}
               onChange={onChangeTimeStart}
+              openTo={"hours"}
+              views={["hours"]}
             />
             <p>&ndash;</p>
-            <KeyboardTimePicker
+            <TimePicker
               label=""
               placeholder="00:00"
               ampm={false}
-              mask="__:__"
               value={valueTimeEnd}
               onChange={onChangeTimeEnd}
+              openTo={"hours"}
+              views={["hours"]}
             />
           </MuiPickersUtilsProvider>
         </div>
         <br />
 
         <h4>Skrót zmiany</h4>
-        <TextField type="text" placeholder="Skrót" />
+        <TextField
+          type="text"
+          placeholder="Skrót"
+          value={shiftCode}
+          onChange={(event): void => setShiftCode(event.target.value)}
+        />
         <br />
 
         <h4>Kolor zmiany</h4>
         <ColorSelector
-          shiftType={value}
+          shiftType={valueRadio}
           mainLabel="Wybierz kolory"
           variant="outlined"
           position={"bottom"}
@@ -88,8 +114,13 @@ export default function ShiftEditDrower(): JSX.Element {
           className="submit-button"
           variant="primary"
           onClick={(): void => {
-            setNewShift({ code: "", name: "", from: 0, to: 12, color: colorPicked });
-            console.log(newShift);
+            setNewShift({
+              code: shiftCode,
+              name: shiftName,
+              from: valueTimeStart?.getHours() ?? 0,
+              to: valueTimeEnd?.getHours() ?? 0,
+              color: colorPicked,
+            });
           }}
         >
           Dodaj zmianę
