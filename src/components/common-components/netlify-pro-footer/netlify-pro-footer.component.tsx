5904764a4807acbd175bc6061e9e3e8911325bc4
Maciej Bielech <maciej.bielech@u2i.com>
2021-01-18 22:13:02 +0100

    TASK-206 Add netlify pro open source project footer and code of conduct

---
+++ src/components/common-components/netlify-pro-footer/netlify-pro-footer.component.tsx
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import ScssVars from "../../../assets/styles/styles/custom/_variables.module.scss";
+
+const useStyles = makeStyles({
+  footer: {
+    background: ScssVars.white,
+    padding: 20,
+    borderTop: "1px solid #E9EEF9",
+    width: "100%",
+    color: ScssVars.primary,
+  },
+});
+
+export function NetlifyProFooter(): JSX.Element {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.footer} justify="space-between" alignItems="center">
+      <Grid item>Wszelkie prawa zastrzeżone. Copyright © 2020-2021: Glider</Grid>
+      <Grid item>
+        <a href="https://www.netlify.com">
+          <img
+            src="https://www.netlify.com/img/global/badges/netlify-light.svg"
+            alt="Deploys by Netlify"
+          />
+        </a>
+      </Grid>
+    </Grid>
+  );
+}
