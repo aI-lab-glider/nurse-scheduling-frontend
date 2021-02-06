0b68d30875ff7e38660daba7ff36c569eca949a0
Tomasz Pecak <pecatoma@gmail.com>
2021-02-06 12:50:40 +0100

    Forbid to change schedule revision to actual on future month

--- src/components/schedule-page/view-only-page/view-only-toolbar.tsx
+++ src/components/schedule-page/view-only-page/view-only-toolbar.tsx
@@ -1,16 +1,32 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
+import React, { useEffect } from "react";
 import { ImportButtonsComponent } from "../import-buttons/import-buttons.component";
 import { Link } from "react-router-dom";
 import { Button } from "../../common-components";
+import { useSelector } from "react-redux";
+import { ApplicationStateModel } from "../../../state/models/application-state.model";
 
 interface ViewOnlyToolbarOptions {
   openEdit: () => void;
 }
 export function ViewOnlyToolbar({ openEdit }: ViewOnlyToolbarOptions): JSX.Element {
   const [revisionType, setRevisionType] = React.useState<string>("actual");
+  const [isScheduleMonthInFuture, setIsScheduleMonthInFuture] = React.useState<boolean>(false);
+  const { year, month_number: month } = useSelector(
+    (state: ApplicationStateModel) => state.actualState.persistentSchedule.present.schedule_info
+  );
+
+  useEffect(() => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    if (currentYear > year || (currentYear === year && currentMonth >= month)) {
+      setIsScheduleMonthInFuture(true);
+    } else {
+      setIsScheduleMonthInFuture(false);
+    }
+  }, [year, month]);
 
   const handleChange = (event: React.ChangeEvent<{ name?: string; value: string }>) => {
     setRevisionType(event.target.value);
@@ -18,17 +34,23 @@ export function ViewOnlyToolbar({ openEdit }: ViewOnlyToolbarOptions): JSX.Eleme
 
   return (
     <div className="buttons">
-      <form>
-        <select
-          value={revisionType}
-          onChange={handleChange}
-          className="revision-select"
-          data-cy="revision-select"
-        >
-          <option value="primary">wersja podstawowa</option>
-          <option value="actual">wersja aktualna</option>
-        </select>
-      </form>
+      <div className="revision-type-container">
+        {isScheduleMonthInFuture ? (
+          <form>
+            <select
+              value={revisionType}
+              onChange={handleChange}
+              className="revision-select"
+              data-cy="revision-select"
+            >
+              <option value="primary">wersja podstawowa</option>
+              <option value="actual">wersja aktualna</option>
+            </select>
+          </form>
+        ) : (
+          <p>wersja podstawowa</p>
+        )}
+      </div>
       <div className="filler" />
       <ImportButtonsComponent />
       <Link to="/schedule-editing">
