8bf9054d6ece083f0f5e1f988b0c53caffac717e
Tomasz Pecak <pecatoma@gmail.com>
2021-02-09 00:16:55 +0100

    Disable month switch in edit mode

--- src/components/common-components/header/header.component.tsx
+++ src/components/common-components/header/header.component.tsx
@@ -1,17 +1,23 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MonthSwitchComponent } from "../month-switch/month-switch.component";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import { Button } from "../button-component/button.component";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 interface HeaderCheckInterface {
   isNewMonthPage: boolean;
 }
 
 export function HeaderComponent({ isNewMonthPage }: HeaderCheckInterface): JSX.Element {
+  const location = useLocation();
+  const [isInEditMode, setIsInEditMode] = useState(false);
+  useEffect(() => {
+    setIsInEditMode(location.pathname === "/schedule-editing");
+  }, [location]);
   return (
     <>
       <div id={"header"}>
@@ -24,7 +30,7 @@ export function HeaderComponent({ isNewMonthPage }: HeaderCheckInterface): JSX.E
           </Link>
         )}
         <div className={"filler"} />
-        <MonthSwitchComponent />
+        {!isInEditMode && <MonthSwitchComponent />}
         <div className={"filler"} />
       </div>
     </>
