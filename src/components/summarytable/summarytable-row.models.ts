3592e8fc97d0af8d1b9e91d8ac99cc8a61680b47
Tomasz Pęcak <pecatoma@gmail.com>
2021-02-14 16:22:29 +0100

    Task 236 license header linter (#179)

--- src/components/summarytable/summarytable-row.models.ts
+++ src/components/summarytable/summarytable-row.models.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import { baseRowDataCy } from "../schedule-page/table/schedule/schedule-parts/base-row.models";
 
 export const summaryRowDataCy = (cellIndex: number): string => baseRowDataCy(cellIndex);
--- src/components/workers-page/shifts-tab/shift-edit-drawer.component.tsx
+++ src/components/workers-page/shifts-tab/shift-edit-drawer.component.tsx
@@ -1,6 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import TextField from "@material-ui/core/TextField";
 import React, { useState } from "react";
-import { Button } from "../../common-components/button-component/button.component";
+import { Button } from "../../common-components";
 import { ColorSelector } from "../../common-components/color-selector/color-selector.component";
 import { MuiPickersUtilsProvider, TimePicker } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
