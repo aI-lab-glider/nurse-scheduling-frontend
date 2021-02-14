3e190c865a61cb34678730d8543e3a4991695ec5
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-02-14 16:06:22 +0100

    Adds license headers to new files

--- src/components/schedule-page/table/schedule/schedule-parts/base-cell/base-cell.models.ts
+++ src/components/schedule-page/table/schedule/schedule-parts/base-cell/base-cell.models.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import { VerboseDate } from "../../../../../../common-models/month-info.model";
 import { CellColorSet } from "../../../../../../helpers/colors/cell-color-set.model";
 import { BaseCellInputOptions } from "./base-cell-input.component";
