f81c6f0abd1878da1ebfdd49d5ba2257bb09737c
Bohdan <qwebeck5@gmail.com>
2021-02-19 22:44:03 +0100

    Fixes extend method (#187)

--- src/components/namestable/worker-info.component.tsx
+++ src/components/namestable/worker-info.component.tsx
@@ -1,16 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
-import {
-  WorkerInfoModel,
-  WorkerType,
-  WorkerTypeHelper,
-} from "../../common-models/worker-info.model";
-import { StringHelper } from "../../helpers/string.helper";
-import WorkersCalendar from "../workers-page/workers-calendar/workers-calendar.component";
 import { Divider } from "@material-ui/core";
 import classNames from "classnames/bind";
+import React from "react";
+import { WorkerInfoModel, WorkerTypeHelper } from "../../common-models/worker-info.model";
+import { StringHelper } from "../../helpers/string.helper";
+import WorkersCalendar from "../workers-page/workers-calendar/workers-calendar.component";
 
 export function WorkerInfoComponent(info: WorkerInfoModel): JSX.Element {
   return (
