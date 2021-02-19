af20dfe2e697e2bd3e39ff2f5dac531846f6ef6f
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-02-20 00:03:00 +0100

    Fixes bug with disappearing modal

--- src/components/schedule-page/schedule-page.component.tsx
+++ src/components/schedule-page/schedule-page.component.tsx
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
+import React, { useCallback } from "react";
 import { Route, Switch } from "react-router-dom";
 import { ScheduleEditPage } from "./edit-page/schedule-edit.page";
 import { ScheduleViewOnlyPage } from "./view-only-page/schedule-view-only.page";
@@ -10,22 +10,24 @@ interface SchedulePageOptions {
   editModeHandler: (editMode: boolean) => void;
 }
 
-export function SchedulePage(props: SchedulePageOptions): JSX.Element {
-  function ViewOnly(): JSX.Element {
-    return (
+export function SchedulePage({ editModeHandler }: SchedulePageOptions): JSX.Element {
+  const ViewOnly = useCallback(
+    (): JSX.Element => (
       <>
-        <ScheduleViewOnlyPage openEdit={(): void => props.editModeHandler(true)} />
+        <ScheduleViewOnlyPage openEdit={(): void => editModeHandler(true)} />
       </>
-    );
-  }
+    ),
+    [editModeHandler]
+  );
 
-  function Edit(): JSX.Element {
-    return (
+  const Edit = useCallback(
+    (): JSX.Element => (
       <>
-        <ScheduleEditPage closeEdit={(): void => props.editModeHandler(false)} />
+        <ScheduleEditPage closeEdit={(): void => editModeHandler(false)} />
       </>
-    );
-  }
+    ),
+    [editModeHandler]
+  );
 
   return (
     <>
