a4a0cdbb456d17edbe9e857c00a3514d69462218
Ehevi <aga.cyra@wp.pl>
2021-02-20 22:31:36 +0100

    TASK-247-getting-rid-of-warnings

--- src/components/schedule-page/schedule-page.component.tsx
+++ src/components/schedule-page/schedule-page.component.tsx
@@ -22,18 +22,17 @@ export function SchedulePage({ editModeHandler }: SchedulePageOptions): JSX.Elem
     [editModeHandler]
   );
 
-  function handleEditButton(): void {
-    editModeHandler(false);
-    setDrawerOpen(false);
-  }
-  const Edit = useCallback(
-    (): JSX.Element => (
+  const Edit = useCallback((): JSX.Element => {
+    function handleEditButton(): void {
+      editModeHandler(false);
+      setDrawerOpen(false);
+    }
+    return (
       <>
         <ScheduleEditPage closeEdit={handleEditButton} />
       </>
-    ),
-    [handleEditButton]
-  );
+    );
+  }, [editModeHandler, setDrawerOpen]);
 
   return (
     <>
