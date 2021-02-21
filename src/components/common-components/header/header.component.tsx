5eb6401128c6bc579a9fac9135c6dee4c958d948
Ehevi <aga.cyra@wp.pl>
2021-02-21 15:43:08 +0100

    getting rid of warnings

--- src/components/common-components/header/header.component.tsx
+++ src/components/common-components/header/header.component.tsx
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React, { useState } from "react";
+import React from "react";
 import { MonthSwitchComponent } from "../month-switch/month-switch.component";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import { Button } from "../button-component/button.component";
