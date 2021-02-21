bebf4775b1f11073811b7268237809baccd3119a
Bohdan Forostianyi <qwebeck5@gmail.com>
2021-02-21 22:43:29 +0100

    Adds backend compatible shifts

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -4,9 +4,13 @@
 import axios, { AxiosInstance } from "axios";
 import { ScheduleDataModel } from "../common-models/schedule-data.model";
 import { ScheduleError } from "../common-models/schedule-error.model";
+import { Shift, SHIFTS } from "../common-models/shift-info.model";
 
 interface BackendErrorObject extends Omit<ScheduleError, "kind"> {
   code: string;
+}
+interface BackendShiftModel extends Shift {
+  is_working_shift: boolean;
 }
 
 class Backend {
@@ -23,6 +27,14 @@ class Backend {
   }
 
   public getErrors(schedule: ScheduleDataModel): Promise<ScheduleError[]> {
+    /* eslint-disable @typescript-eslint/camelcase */
+    schedule.shift_types = {};
+    Object.keys(SHIFTS).forEach((shiftCode) => {
+      schedule.shift_types![shiftCode] = {
+        ...SHIFTS[shiftCode],
+        is_working_shift: SHIFTS[shiftCode].isWorkingShift,
+      };
+    });
     return this.sleep(1000).then(() =>
       this.axios
         .post("/schedule_errors", schedule)
