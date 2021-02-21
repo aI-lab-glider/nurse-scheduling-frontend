ca178932e2e0c310b4c5b4c6b2a438b36cc1c9ea
Ehevi <aga.cyra@wp.pl>
2021-02-21 15:52:18 +0100

    a little update

--- src/components/common-components/header/header.component.tsx
+++ src/components/common-components/header/header.component.tsx
@@ -1,13 +1,17 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
+import React, { useState } from "react";
 import { MonthSwitchComponent } from "../month-switch/month-switch.component";
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import { Button } from "../button-component/button.component";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { ApplicationStateModel } from "../../../state/models/application-state.model";
+import { useScreenshot } from "use-screenshot-hook";
+import ReportIssueModal from "../modal/report-issue-modal/report-issue-modal.component";
+import { Typography } from "@material-ui/core";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 interface HeaderCheckInterface {
   isNewMonthPage: boolean;
@@ -16,6 +20,13 @@ interface HeaderCheckInterface {
 export function HeaderComponent({ isNewMonthPage }: HeaderCheckInterface): JSX.Element {
   const { mode } = useSelector((state: ApplicationStateModel) => state.actualState);
   const isReadOnlyMode = mode === "readonly";
+  const [modalOpen, modalSetOpen] = useState(false);
+  const { image, takeScreenshot, clear } = useScreenshot();
+
+  function onReportIssueClick(): void {
+    takeScreenshot();
+    modalSetOpen(true);
+  }
 
   return (
     <>
@@ -31,6 +42,16 @@ export function HeaderComponent({ isNewMonthPage }: HeaderCheckInterface): JSX.E
         <div className={"filler"} />
         {isReadOnlyMode && <MonthSwitchComponent />}
         <div className={"filler"} />
+        <Link onClick={(): void => onReportIssueClick()}>
+          <Typography className={"reportIssueLink"}>Zgłoś błąd</Typography>
+        </Link>
+        <ReportIssueModal
+          open={modalOpen}
+          setOpen={modalSetOpen}
+          screenshot={image}
+          clear={clear}
+        />
+        <SettingsIcon />
       </div>
     </>
   );
