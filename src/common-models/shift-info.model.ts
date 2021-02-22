039b781a74ec59cba305846060d988e1ef6a7dad
Bohdan <qwebeck5@gmail.com>
2021-02-22 23:32:46 +0100

    TASK-264 Hardcode pandemic shifts (#199)
    * Adds new shifts
    
    * Fixes tests
    
    * Adds backend compatible shifts
    
    * Fixes tests
    
    * bug+colors (#203)
    
    * bug+colors
    
    * bug+colors
    
    Co-authored-by: Paweł Renc <rencpawe@gmail.com>
    Co-authored-by: Paulina <kaplonpaulina97@gmail.com>

--- src/common-models/shift-info.model.ts
+++ src/common-models/shift-info.model.ts
@@ -12,8 +12,102 @@ export interface Shift {
   isWorkingShift?: boolean;
 }
 
-export const shifts: { [id: string]: Shift } = {
-  R: { code: "R", name: "Rano", from: 7, to: 15, color: "FFD100", isWorkingShift: true },
+export enum ShiftCode {
+  RP = "RP",
+  RPN = "RPN",
+  N8 = "N8",
+  RN8 = "RN8",
+  DN8 = "DN8",
+  D1 = "D1",
+  D2 = "D2",
+  P1 = "P1",
+  R1 = "R1",
+  R = "R",
+  P = "P",
+  D = "D",
+  N = "N",
+  DN = "DN",
+  PN = "PN",
+  W = "W",
+  U = "U",
+  L4 = "L4",
+  K = "K",
+}
+export const SHIFTS: { [code in ShiftCode]: Shift } = {
+  RP: {
+    code: "RP",
+    name: "Rano popłudnie",
+    from: 7,
+    to: 19,
+    color: "FFD100",
+    isWorkingShift: true,
+  },
+  RPN: {
+    code: "RPN",
+    name: "Rano, popołudni, noc",
+    from: 7,
+    to: 7,
+    color: "9025cf",
+    isWorkingShift: true,
+  },
+  N8: {
+    code: "N8",
+    name: "Noc na 8 godzin",
+    from: 23,
+    to: 7,
+    color: "76a877",
+    isWorkingShift: true,
+  },
+  RN8: {
+    code: "RN8",
+    name: "Noc na 8 godzin + Rano",
+    from: 22,
+    to: 15,
+    color: "805646",
+    isWorkingShift: true,
+  },
+  DN8: {
+    code: "DN8",
+    name: "Dzień + noc 8 godzin",
+    from: 22,
+    to: 19,
+    color: "c9592e",
+    isWorkingShift: true,
+  },
+  D1: {
+    code: "D1",
+    name: "Dzień 1",
+    from: 7,
+    to: 17,
+    color: "396e75",
+    isWorkingShift: true,
+  },
+  D2: {
+    code: "D2",
+    name: "Dzień 2",
+    from: 7,
+    to: 16,
+    color: "eda81c",
+    isWorkingShift: true,
+  },
+  P1: {
+    code: "P1",
+    name: "Popupułudnie 1",
+    from: 15,
+    to: 21,
+    color: "2003fc",
+    isWorkingShift: true,
+  },
+  R1: {
+    code: "R1",
+    name: "Rano 1",
+    from: 7,
+    to: 13,
+    color: "5ce6dc",
+    isWorkingShift: true,
+  },
+
+  R: { code: "R", name: "Rano", from: 7, to: 15, color: "a82758", isWorkingShift: true },
   P: { code: "P", name: "Popołudnie", from: 15, to: 19, color: "00A3FF", isWorkingShift: true },
   D: { code: "D", name: "Dzień", from: 7, to: 19, color: "73B471", isWorkingShift: true },
   N: { code: "N", name: "Noc", from: 19, to: 7, color: "1D3557", isWorkingShift: true },
@@ -21,7 +115,7 @@ export const shifts: { [id: string]: Shift } = {
   PN: {
     code: "PN",
     name: "Popołudnie + Noc",
-    from: 19,
+    from: 15,
     to: 7,
     isWorkingShift: true,
   },
@@ -51,19 +145,6 @@ export const shifts: { [id: string]: Shift } = {
     isWorkingShift: false,
   },
 };
-
-export enum ShiftCode {
-  R = "R",
-  P = "P",
-  D = "D",
-  N = "N",
-  DN = "DN",
-  PN = "PN",
-  W = "W",
-  U = "U",
-  L4 = "L4",
-  K = "K",
-}
 
 export interface ShiftInfoModel {
   [nurseName: string]: ShiftCode[];
