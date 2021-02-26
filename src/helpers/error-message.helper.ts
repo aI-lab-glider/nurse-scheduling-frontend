a940d406e4905ac78ad74bf6273d32d4d1a51628
Szymon Fugas <38334396+Sig00rd@users.noreply.github.com>
2021-02-26 01:29:12 +0100

    TASK-281 Fix dates of discovered issues (#219)
    * Add nurse count errors to table header, fix indexing for errors shown at foundation info
    
    * TASK-293 remove error badges from foundation info comp altogether
    
    * TASK-281 tinker with error days indexing and presentation
    
    * Sort errors based on date
    
    * Increase timeout on getting worker shift
    
    * Increase timeouts even further beyond
    
    * Fix outdated message asserts in schedule errors test
    
    * Fix message asserts
    
    * Increase timeout on schedule error folding section get
    
    * Update messages in schedule-errors test
    
    * Update schedule-errors.spec.ts
    
    * Update commands.ts
    
    * Update schedule-errors.spec.ts
    
    * Update schedule-errors.spec.ts
    
    Co-authored-by: Paweł Renc <rencpawe@gmail.com>

--- src/helpers/error-message.helper.ts
+++ src/helpers/error-message.helper.ts
@@ -48,7 +48,7 @@ export class ErrorMessageHelper {
     switch (error.kind) {
       case AlgorithmErrorCode.AlwaysAtLeastOneNurse:
         i = 0;
-        message = `Brak pielęgniarek w dniu <strong>${error.day} na zmianie ${
+        message = `Brak pielęgniarek w dniu <strong>${error.day + 1} na zmianie ${
           error.day_time ? dayTimeTranslations[error.day_time] : ""
         }</strong>`;
         if (error.segments[i][0] !== 1 && error.segments[i][1] !== 24) {
@@ -68,7 +68,7 @@ export class ErrorMessageHelper {
         break;
       case AlgorithmErrorCode.WorkerNumberDuringDay:
         i = 0;
-        message = `Za mało pracowników w trakcie dnia w dniu <strong>${error.day}</strong>`;
+        message = `Za mało pracowników w trakcie dnia w dniu <strong>${error.day + 1}</strong>`;
         if (error.segments && error.segments[i][0] !== 1 && error.segments[i][1] !== 24) {
           message += ` w godzinach <strong>${error.segments[i][0]}-${error.segments[i][1]}</strong>`;
           while (error.segments[i + 1]) {
@@ -87,7 +87,7 @@ export class ErrorMessageHelper {
         break;
       case AlgorithmErrorCode.WorkerNumberDuringNight:
         i = 0;
-        message = `Za mało pracowników w nocy w dniu <strong>${error.day}</strong>`;
+        message = `Za mało pracowników w nocy w dniu <strong>${error.day + 1}</strong>`;
         if (error.segments && error.segments[i][0] !== 22 && error.segments[i][1] !== 6) {
           message += ` w godzinach <strong>${error.segments[i][0]}-${error.segments[i][1]}</strong>`;
           while (error.segments[i + 1]) {
@@ -105,7 +105,11 @@ export class ErrorMessageHelper {
         }
         break;
       case AlgorithmErrorCode.DissalowedShiftSequence:
-        message = `Niedozwolona sekwencja zmian dla pracownika <strong>${error.worker}</strong> w dniu <strong>${error.day}</strong>: <strong>${error.succeeding}</strong> po <strong>${error.preceding}</strong>`;
+        message = `Niedozwolona sekwencja zmian dla pracownika <strong>${
+          error.worker
+        }</strong> w dniu <strong>${error.day + 1}</strong>: <strong>${
+          error.succeeding
+        }</strong> po <strong>${error.preceding}</strong>`;
         type = ScheduleErrorType.DSS;
         title = "date";
         if (error.day) {
@@ -113,7 +117,9 @@ export class ErrorMessageHelper {
         }
         break;
       case AlgorithmErrorCode.LackingLongBreak:
-        message = `Brak wymaganej długiej przerwy dla pracownika <strong>${error.worker}</strong> w tygodniu <strong>${error.week}</strong>`;
+        message = `Brak wymaganej długiej przerwy dla pracownika <strong>${
+          error.worker
+        }</strong> w tygodniu <strong>${error.week + 1}</strong>`;
         type = ScheduleErrorType.LLB;
         title = `${error.worker}`;
         break;
@@ -128,7 +134,9 @@ export class ErrorMessageHelper {
         title = `${error.worker}`;
         break;
       case ParseErrorCode.UNKNOWN_VALUE:
-        message = `Nieznana wartość zmiany: "${error.actual}" dla pracownika  ${error.worker} w dniu ${error.day}. Przyjęto, że zmiana to wolne.`;
+        message = `Nieznana wartość zmiany: "${error.actual}" dla pracownika  ${
+          error.worker
+        } w dniu ${error.day! + 1}. Przyjęto, że zmiana to wolne.`;
         type = ScheduleErrorType.ILLEGAL_SHIFT_VALUE;
         title = `${error.worker}`;
         break;
