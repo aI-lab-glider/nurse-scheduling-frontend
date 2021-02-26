18e7190e4a75126f3e53ed33b9bffc311290f418
Paweł Renc <rencpawe@gmail.com>
2021-02-26 02:56:22 +0100

    Update new-month-plan.component.tsx

--- src/components/schedule-page/new-month-plan.component.tsx
+++ src/components/schedule-page/new-month-plan.component.tsx
@@ -74,7 +74,7 @@ export function NewMonthPlanComponent(): JSX.Element {
   return (
     <>
       <div className={"newMonthComponents"}>
-        <img src="https://i.imgur.com/UbHPZYG.jpg" alt="" />
+        <img src="https://imgur.com/2tTcKbj" alt="" />
         <p>Nie masz planu na ten miesiąc</p>
         <div className={"newPageButtonsPane"}>
           {hasValidPrevious && (
