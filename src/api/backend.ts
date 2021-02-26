4fc7ae0144c700d15e738b4df95d9799aa272392
Tomasz Pęcak <pecatoma@gmail.com>
2021-02-26 02:11:46 +0100

    Task-286 Save workers persistently (#222)
    * Fixes worker agreement
    
    * Contract type and worker type are automatically shown when user is edited
    
    * fixes work time calculation for civil contract
    
    * Adds validation to worker drawer
    
    * Adds opacity
    
    * Fixes edit mode and table with workers
    
    * Adds validation for invalid civil time
    
    * TASK-296 Show month label in edit mode (#212)
    
    * Show month label in edit mode, fix disable button opacity
    
    * Disable edit mode button
    
    * Create app config context to store app mode
    
    * Remove link parent component to shrink onClick space
    
    * Put worker logic in action creator
    
    * Refactor schedule action creator
    
    * Save NZ if in current month
    
    Co-authored-by: Bohdan Forostianyi <qwebeck5@gmail.com>
    Co-authored-by: Paweł Renc <rencpawe@gmail.com>

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -4,15 +4,11 @@
 import axios, { AxiosInstance } from "axios";
 import { ScheduleDataModel } from "../common-models/schedule-data.model";
 import { ScheduleError } from "../common-models/schedule-error.model";
-import { Shift, SHIFTS } from "../common-models/shift-info.model";
+import { SHIFTS } from "../common-models/shift-info.model";
 import { v4 as uuidv4 } from "uuid";
 
 interface BackendErrorObject extends Omit<ScheduleError, "kind"> {
   code: string;
-}
-
-interface BackendShiftModel extends Shift {
-  is_working_shift: boolean;
 }
 
 function escapeJuliaIndexes(error: ScheduleError): ScheduleError {
