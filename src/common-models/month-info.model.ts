04616494a136ffc69df98f0d663535fa70e2577f
Tomasz Pecak <pecatoma@gmail.com>
2021-03-10 12:07:07 +0100

    Add validation to schedule and month data model, fix copy function

--- src/common-models/month-info.model.ts
+++ src/common-models/month-info.model.ts
@@ -1,6 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { SCHEDULE_CONTAINERS_LENGTH, ScheduleContainerType } from "./schedule-data.model";
+
 export enum WeekDay {
   MO = "MO",
   TU = "TU",
@@ -24,4 +26,28 @@ export interface MonthInfoModel {
   extra_workers?: number[];
   frozen_shifts: [number | string, number][];
   dates: number[];
+}
+
+export function validateMonthInfo(
+  monthInfo: MonthInfoModel,
+  containerType: ScheduleContainerType
+): void {
+  const scheduleLen = monthInfo.dates.length;
+  if (!SCHEDULE_CONTAINERS_LENGTH[containerType].includes(scheduleLen)) {
+    throw new Error(`Schedule dates have wrong length: ${scheduleLen}`);
+  }
+  if (scheduleLen !== monthInfo.children_number!.length) {
+    throw new Error(
+      `Children number has to be specified for each ${scheduleLen} days not for ${
+        monthInfo.children_number!.length
+      }`
+    );
+  }
+  if (scheduleLen !== monthInfo.extra_workers!.length) {
+    throw new Error(
+      `Extra workers number has to be specified for each ${scheduleLen} days not for ${
+        monthInfo.extra_workers!.length
+      }`
+    );
+  }
 }
