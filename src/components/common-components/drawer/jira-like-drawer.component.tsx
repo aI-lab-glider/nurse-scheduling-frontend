b0adc4304a8d448b35d7ebcd6d698ff5fee818d9
Jan Ślażyński <slazynski.j@gmail.com>
2021-03-17 21:21:20 +0100

    TASK-389 Close issue drawer when exiting edit mode (#285)
    * Bug fixed. Cypress passing.
    
    * Added isEditMode watcher for auto drawer closing.

--- src/components/common-components/drawer/jira-like-drawer.component.tsx
+++ src/components/common-components/drawer/jira-like-drawer.component.tsx
@@ -1,12 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import React from "react";
+import React, { useEffect } from "react";
 import DrawerHeader from "./drawer-header.component";
 import { Box } from "@material-ui/core";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import { useJiraLikeDrawer } from "./jira-like-drawer-context";
 import ScssVars from "../../../assets/styles/styles/custom/_variables.module.scss";
+import { useSelector } from "react-redux";
+import { ApplicationStateModel } from "../../../state/models/application-state.model";
 
 export interface StyleProps {
   width: number;
@@ -25,12 +27,19 @@ const useStyles = makeStyles<Theme, StyleProps>({
 });
 
 export default function JiraLikeDrawer(width): JSX.Element {
+  const isEditMode = useSelector(
+    (state: ApplicationStateModel) => state.actualState.mode === "edit"
+  );
   const classes = useStyles(width);
   const { title, open, setOpen, childrenComponent } = useJiraLikeDrawer();
 
+  useEffect(() => {
+    if (!isEditMode) setOpen(false);
+  }, [isEditMode, setOpen]);
+
   return (
     <Box>
-      {title && open && setOpen && childrenComponent && (
+      {title && open && setOpen && childrenComponent && isEditMode && (
         <Box className={classes.drawer}>
           <DrawerHeader title={title} setOpen={setOpen}>
             {childrenComponent}
