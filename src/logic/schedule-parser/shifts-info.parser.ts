4b9d5bb1a9d0458843b2fdf54360552e47ca4380
Jan Ślażyński <slazynski.j@gmail.com>
2021-04-01 10:24:36 +0200

    TASK-431 Increase dropdown timeout from 444ms to 4444ms (#336)

--- src/logic/schedule-parser/shifts-info.parser.ts
+++ src/logic/schedule-parser/shifts-info.parser.ts
@@ -1,11 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import {
-  InputFileErrorCode,
-  ParseErrorCode,
-  ScheduleError,
-} from "../../common-models/schedule-error.model";
+import { ParseErrorCode, ScheduleError } from "../../common-models/schedule-error.model";
 import { ShiftCode } from "../../common-models/shift-info.model";
 import { WorkerGroup } from "../../common-models/worker-info.model";
 import { StringHelper } from "../../helpers/string.helper";
