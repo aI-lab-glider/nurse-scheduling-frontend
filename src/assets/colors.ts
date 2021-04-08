4029a41c6c3f9b1e018df03dc3c78bc2cd1fb726
Tomasz Pęcak <tomasz.pecak@bshg.com>
2021-04-08 21:01:59 +0200

    Remove autocomplete styles

---
+++ src/assets/colors.ts
@@ -0,0 +1,51 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+export const theme = {
+  linkDisabled: "#666666",
+  transparentBg: "transparent",
+  lightBg: "#f5f6fa",
+  darkBackground: "#555555",
+  primary: "#1d3557",
+  secondary: "#000000",
+  primaryTextColor: "#1d3557",
+  secondaryTextColor: "#000000",
+  primaryBackground: "#e5e5e5",
+  primaryHeaderColor: "#1d3557",
+  headerTextColor: "#ffffff",
+  babysitterColor: "#ffd100",
+  babysitterLabelBackground: "#ffd100",
+  nurseColor: "#c8dfff",
+  nurseLabelBackground: "#c8dfff",
+  cellColor: "#4e4b66",
+  cellWeekendBackgroundColor: "rgba(60, 105, 168, 0.2)",
+  cellWeekendHeaderBackgroundColor: "rgba(60, 105, 168, 0.3)",
+  cellOtherMonthBackgroundColor: "#f5f9ff",
+  cellOtherColor: "rgba(78, 75, 102, 0.5)",
+  headerCurrMonthBackgroundColor: "#ecf4ff",
+  white: "#FFFFFF",
+  gray100: "#f6f9fc",
+  gray200: "#e9ecef",
+  gray300: "#e3e3e3",
+  gray400: "#ced4da",
+  gray500: "#adb5bd",
+  gray600: "#6c757d",
+  gray700: "#525f7f",
+  gray800: "#32325d",
+  gray900: "#212529",
+};
+
+export const fontFamilyPrimary = "Roboto, sans-serif";
+
+export const fontSizeDash = 0.875;
+export const fontSizeBase = 1; // Assumes the browser default, typically `16px`
+export const fontSizeLg = fontSizeBase * 1.142;
+export const fontSizeXl = fontSizeBase * 1.428;
+export const fontSizeSm = fontSizeBase * 0.85714286;
+export const fontSizeXs = fontSizeBase * 0.714;
+
+export const fontWeightLight = 300;
+export const fontWeightNormal = 400;
+export const fontWeightBold = 600;
+export const fontWeightExtra = "bold";
+export const fontWeightBase = fontWeightNormal;
