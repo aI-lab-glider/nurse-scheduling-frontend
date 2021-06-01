250720323d1347a021a66f2b583cb52d73384b27
net-runner <slazynski.j@gmail.com>
2021-06-01 11:35:24 +0200

    Supressed sonarqube error.

--- src/state/schedule-data/worker-info/worker.action-creator.ts
+++ src/state/schedule-data/worker-info/worker.action-creator.ts
@@ -138,8 +138,7 @@ export class WorkerActionCreator {
     delete monthDataModelCopy.employee_info.time[workerName];
     delete monthDataModelCopy.employee_info.type[workerName];
     if (monthDataModelCopy.employee_info.contractType) {
-      //sonarqube ignore
-      delete monthDataModelCopy.employee_info.contractType?.[workerName];
+      delete monthDataModelCopy.employee_info.contractType?.[workerName]; //nosonar
     }
     delete monthDataModelCopy.shifts[workerName];
 
