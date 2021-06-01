9c3f7aa68db91422460e84d8732a2ccbf07f1fd5
net-runner <slazynski.j@gmail.com>
2021-06-01 11:21:49 +0200

    Maybe now the error is ignored.

--- src/state/schedule-data/worker-info/worker.action-creator.ts
+++ src/state/schedule-data/worker-info/worker.action-creator.ts
@@ -138,7 +138,7 @@ export class WorkerActionCreator {
     delete monthDataModelCopy.employee_info.time[workerName];
     delete monthDataModelCopy.employee_info.type[workerName];
     if (monthDataModelCopy.employee_info.contractType) {
-      //ignore
+      //sonarqube ignore
       delete monthDataModelCopy.employee_info.contractType?.[workerName];
     }
     delete monthDataModelCopy.shifts[workerName];
