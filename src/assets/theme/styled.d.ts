95cab14e9f182c0173e0f9919479a9f0206c42c8
net-runner <slazynski.j@gmail.com>
2021-06-20 09:56:46 +0200

    Removed obsolete eslint ignore.

--- src/assets/theme/styled.d.ts
+++ src/assets/theme/styled.d.ts
@@ -6,6 +6,5 @@ import "styled-components";
 import { Theme } from ".";
 
 declare module "styled-components" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme {} // extends the global DefaultTheme with our ThemeType.
+  export type DefaultTheme = Theme;
 }
