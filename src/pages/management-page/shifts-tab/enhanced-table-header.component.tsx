4298b6a067c5f0114fa99ca27f4eae1d45b6c39a
net-runner <slazynski.j@gmail.com>
2022-04-30 18:06:55 +0200

    .... sc 👍

--- src/pages/management-page/shifts-tab/enhanced-table-header.component.tsx
+++ src/pages/management-page/shifts-tab/enhanced-table-header.component.tsx
@@ -33,26 +33,6 @@ export function EnhancedTableHeaderComponent(props: EnhancedTableProps): JSX.Ele
         {headCells.map((headCell) => (
           <S.TableCell key={headCell.id}>{headCell.label}</S.TableCell>
         ))}
-        <S.TableCell align="right">
-          {/* <S.HeaderButton
-            variant="primary"
-            disabled
-            onClick={(): void => {
-              toggleOpen(
-                {
-                  name: "Nowa zmiana",
-                  from: 0,
-                  to: 0,
-                  color: "FFD100",
-                  isWorkingShift: true,
-                },
-                ShiftEditComponentMode.ADD_NEW
-              );
-            }}
-          >
-            Dodaj zmianę
-          </S.HeaderButton> */}
-        </S.TableCell>
       </S.TableRow>
     </TableHead>
   );
