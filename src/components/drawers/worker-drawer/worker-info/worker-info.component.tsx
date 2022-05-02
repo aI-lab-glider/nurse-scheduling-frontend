549737bd1cbd87af14d6ca2721c0cfa11cb73a62
net-runner <slazynski.j@gmail.com>
2022-05-02 22:05:05 +0200

    Styling for worker drawer info finished.

--- src/components/drawers/worker-drawer/worker-info/worker-info.component.tsx
+++ src/components/drawers/worker-drawer/worker-info/worker-info.component.tsx
@@ -4,14 +4,11 @@
 import React from "react";
 import _ from "lodash";
 import * as S from "./worker-info.styled";
-import { colors } from "../../../../assets/css-consts";
 import { ContractTypeHelper } from "../../../../helpers/contract-type.helper";
 import { StringHelper } from "../../../../helpers/string.helper";
-import { WorkerTypeHelper } from "../../../../helpers/worker-type.helper";
 import { useWorkerHoursInfo } from "../../../../hooks/use-worker-hours-info";
 import { useWorkerInfo, WorkerInfo } from "../../../../hooks/use-worker-info";
 import { WorkerName } from "../../../../state/schedule-data/schedule-sensitive-data.model";
-import { WorkerType } from "../../../../state/schedule-data/worker-info/worker-info.model";
 import WorkersCalendar from "../../../workers-calendar/workers-calendar.component";
 import { exportToXlsx } from "./export-to-xlsx";
 import { useMonthInfo } from "../../../../hooks/use-month-info";
@@ -47,36 +44,45 @@ export function WorkerInfoComponent({ workerName }: WorkerInfoComponentOptions):
   };
 
   return (
-    <S.WorkerInfoContainer>
-      <div id={workerInfoExport}>
-        <S.HeaderRow>
+    <S.WorkerInfoContainer id={workerInfoExport}>
+      <S.HeaderRow>
+        <S.NameLabelRow>
           <S.WorkerNameLabel>
             {StringHelper.capitalizeEach(workerInfo.workerName)}
           </S.WorkerNameLabel>
           <WorkerTypeLabel style={{ marginLeft: "10px" }} workerType={workerInfo.workerType} />
-          <S.DownloadButton
-            variant="primary"
-            onClick={(): void =>
-              handleExportAsXlsx(workerInfo, workerHoursInfo, workerInfo.workerShifts)
-            }
-          >
-            {t("downloadWorkerSchedule")}
-          </S.DownloadButton>
-        </S.HeaderRow>
+        </S.NameLabelRow>
 
-        <div>
-          {workerInfo.contractType && (
-            <S.WorkerInfo>
-              Typ umowy: {ContractTypeHelper.translate(workerInfo.contractType)}
-            </S.WorkerInfo>
-          )}
-          <S.WorkerInfo>Liczba godzin: {workerHoursInfo.workerHourNorm}</S.WorkerInfo>
-          <S.WorkerInfo>Liczba nadgodzin: {workerHoursInfo.overTime}</S.WorkerInfo>
-          <S.WorkerInfo>Suma godzin: {workerHoursInfo.workerTime}</S.WorkerInfo>
-          <S.CalendarDivider data-html2canvas-ignore="true" />
-        </div>
-        <WorkersCalendar id={calendarExport} workerShifts={workerInfo.workerShifts} />
-      </div>
+        <S.DownloadButton
+          variant="primary"
+          onClick={(): void =>
+            handleExportAsXlsx(workerInfo, workerHoursInfo, workerInfo.workerShifts)
+          }
+        >
+          {t("downloadWorkerSchedule")}
+        </S.DownloadButton>
+      </S.HeaderRow>
+
+      {workerInfo.contractType && (
+        <S.WorkerInfo>
+          {t("contractTypeExportHeader")}:
+          <S.BoldInfo>
+            {ContractTypeHelper.translate(workerInfo.contractType)} 1/
+            {workerInfo.workerTime}
+          </S.BoldInfo>
+        </S.WorkerInfo>
+      )}
+      <S.WorkerInfo>
+        {t("workerHourNorm")}: <S.BoldInfo>{workerHoursInfo.workerHourNorm}</S.BoldInfo>
+      </S.WorkerInfo>
+      <S.WorkerInfo>
+        {t("overTime")}: <S.OvertimeHours>{workerHoursInfo.overTime}</S.OvertimeHours>
+      </S.WorkerInfo>
+      <S.WorkerInfo>
+        {t("workerTime")}: <S.BoldInfo>{workerHoursInfo.workerTime}</S.BoldInfo>
+      </S.WorkerInfo>
+
+      <WorkersCalendar id={calendarExport} workerShifts={workerInfo.workerShifts} />
     </S.WorkerInfoContainer>
   );
 }
--- src/components/workers-calendar/workers-calendar.styled.ts
+++ src/components/workers-calendar/workers-calendar.styled.ts
@@ -4,6 +4,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  margin-top: 20px;
   width: 100%;
 `;
 
